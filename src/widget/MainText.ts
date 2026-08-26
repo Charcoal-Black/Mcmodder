afd51ebf1ffa035502a10349f1c5ff662661b791
Charcoal-Black <46296828+Charcoal-Black@users.noreply.github.com>
2026-08-26 15:33:11 +0800

    优化图像本地化检测逻辑与提示

--- src/widget/MainText.ts
+++ src/widget/MainText.ts
@@ -107,16 +107,21 @@ export class McmodderMainText {
   }
 
   private singleImageLocalizedCheck(img: HTMLImageElement) {
-    const src = img.src;
+    const src = img.dataset.src ?? img.src;
     fetch(src, { method: "HEAD" }).then(resp => {
+      const container = $(img).parent();
       if (resp.status != 200)
         return;
-      if (Number(resp.headers.get("content-length")) > 1024000)
-        return; // editor.options.fileMaxSize
-      if (!["image/png", "image/jpg", "image/jpeg", "image/gif"].includes(resp.headers.get("content-type") || ""))
-        return; // editor.options.fileAllowFiles ?
-      if (!src.includes("mcmod.cn")) $(img).parent()
-        .append('<span class="mcmodder-common-danger" style="display: inherit;">该图片尚未本地化！</span>')
+      const size = Number(resp.headers.get("content-length"));
+      const contentType = resp.headers.get("content-type") ?? "?";
+      if (size > 1024000) container // editor.options.fileMaxSize
+        .append(`<span class="badge badge-warning" style="display: inherit;">该图片尚未本地化，但是体积 (${ McmodderUtils.getFormattedSize(size) }) 超过了本地图床最大体积限制</span>`)
+        .css("border", "10px solid var(--mcmodder-color-warning)");
+      else if (!["image/png", "image/jpg", "image/jpeg", "image/gif"].includes(contentType)) container // editor.options.fileAllowFiles ?
+        .append(`<span class="badge badge-warning" style="display: inherit;">该图片尚未本地化，但是使用了本地图床不支持的文件格式 (${ contentType })</span>`)
+        .css("border", "10px solid var(--mcmodder-color-warning)");
+      else if (!src.includes("mcmod.cn")) container
+        .append('<span class="badge badge-danger" style="display: inherit;">该图片尚未本地化！</span>')
         .css("border", "10px solid var(--mcmodder-color-danger)");
     });
   }
